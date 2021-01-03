cf5d0a4c43cb5aa17224bb2e8bde956771086ae8
jakub_izbicki <jakub.izbicki1@gmail.com>
2021-01-03 14:10:22 +0100

    Introduce js ration container

--- src/components/CardQuestion.vue
+++ src/components/CardQuestion.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="stretched-content-centered flex-col">
-    <Card :promised-card-data="promisedCardDatas[0]"></Card>
+    <RatioContainer :ratio="containerRatio">
+      <Card :promised-card-data="promisedCardDatas[0]"></Card>
+    </RatioContainer>
     <QuestionButtons :promised-card-datas="promisedCardDatas"></QuestionButtons>
   </div>
 </template>
@@ -10,13 +12,17 @@ import {Vue, Component, Prop} from "vue-property-decorator"
 import Card from "@/components/Card.vue";
 import PromisedCardData from "@/domain/PromisedCardData";
 import QuestionButtons from "@/components/QuestionButtons.vue";
+import RatioContainer from "@/components/ratio/RatioContainer.vue";
+import {RatioConstants} from "@/domain/ratio/Ratio";
 
 @Component({
-  components: {QuestionButtons, Card}
+  components: {RatioContainer, QuestionButtons, Card}
 })
 export default class CardQuestion extends Vue {
 
   @Prop({required: true})
   private promisedCardDatas!: PromisedCardData[];
+
+  private containerRatio = RatioConstants.RATIO_63_88;
 }
 </script>
---
+++ src/components/ratio/RatioContainer.vue
@@ -0,0 +1,69 @@
+<template>
+  <div ref="container"
+       class="stretched relative">
+    <div class="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center">
+      <div class="relative flex flex-col"
+           :style="[{width: `${dimensions.width}px`},
+                    {height: `${dimensions.height}px`},
+                    // {padding: `${paddingY} ${paddingX} ${paddingY} ${paddingX}`}
+                    ]">
+        <slot/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from "vue-property-decorator";
+import Ratio, {DimensionsPx, RatioConstants} from "@/domain/ratio/Ratio";
+
+@Component
+export default class RatioContainer extends Vue {
+
+  @Prop({required: true})
+  private readonly ratio!: RatioConstants;
+
+  private height = 0;
+
+  private width = 0;
+
+  private observer!: MutationObserver;
+
+  mounted() {
+    /* eslint-disable */
+    // @ts-ignore
+    this.observer = new ResizeObserver(singletonArray => {
+      singletonArray.forEach((wrapper: any) => this.setDimensions(wrapper.contentRect));
+    });
+
+    // @ts-ignore
+    this.observer.observe(this.$refs.container);
+    /* eslint-enable */
+  }
+
+  get dimensions(): DimensionsPx {
+    return {
+      height: this.height,
+      width: this.width
+    };
+  }
+
+  private setDimensions(rect: DOMRect): void {
+    if (rect && rect.height && rect.width) {
+      const dimensionsPx = Ratio.trimIntoRatio(rect.height, rect.width, this.ratio);
+
+      this.height = dimensionsPx.height;
+      this.width = dimensionsPx.width;
+    }
+  }
+
+  // get paddingX(): string {
+  // return `${RelativeFontSize.DEFAULT.valueOf() * this.width / 15}px`;
+  // return "0px";
+  // }
+
+  // get paddingY(): string {
+  //   return `${this.width / 45}px`;
+  // }
+}
+</script>
