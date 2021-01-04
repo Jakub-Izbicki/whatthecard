b8b83ddefc7fa28fc19eb40ff9558a06d0cebd45
Jakub Izbicki <jakub.izbicki1@gmail.com>
2021-01-04 12:56:47 +0100

    Add preloading images

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-dark rounded-2xl shadow-card overflow-hidden">
-    <img v-if="!showCard && cardData.length" class="rounded-t-2xl" :src="cardData[0].image_uris.art_crop"
+    <img v-if="!showCard && dataReady" class="rounded-t-2xl" :src="cardData[0].image_uris.art_crop"
          @click="toggleShowCard">
-    <img v-if="showCard && cardData.length" class="rounded-2xl" :src="cardData[0].image_uris.large"
+    <img v-if="showCard && dataReady" class="rounded-2xl" :src="cardData[0].image_uris.large"
          @click="toggleShowCard">
     <QuestionButtons v-if="!showCard" :promised-card-datas="promisedCardDatas"></QuestionButtons>
   </div>
@@ -26,6 +26,14 @@ export default class Card extends Vue {
 
   private showCard = false;
 
+  private cropImg: string | null = null;
+
+  private largeImg: string | null = null;
+
+  get dataReady(): boolean {
+    return !!this.cardData.length && !!this.cropImg && !!this.largeImg;
+  }
+
   private toggleShowCard(): void {
     this.showCard = !this.showCard;
   }
@@ -33,9 +41,18 @@ export default class Card extends Vue {
   mounted() {
     Promise.all(this.promisedCardDatas.map(data => data.get()))
         .then(data => {
-          // console.info(data)
-          this.cardData = data
+          this.cardData = data;
+          this.preloadImages();
         });
+  }
+
+  private preloadImages(): void {
+    const cropImg = new Image();
+    const largeImg = new Image();
+    cropImg.onload = () => this.cropImg = cropImg.src;
+    largeImg.onload = () => this.largeImg = largeImg.src;
+    cropImg.src = this.cardData[0].image_uris.art_crop;
+    largeImg.src = this.cardData[0].image_uris.large;
   }
 }
 </script>
