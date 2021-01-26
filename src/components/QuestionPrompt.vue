2a625e11e085bc5221e42fa86fc187a77c6a932c
jakub_izbicki <jakub.izbicki1@gmail.com>
2021-01-26 21:42:10 +0100

    Make prompt subtitle absolute

--- src/components/QuestionPrompt.vue
+++ src/components/QuestionPrompt.vue
@@ -1,19 +1,20 @@
 <template>
-  <div class="text-4xl pb-3">
+  <div class="text-4xl pb-5">
     <transition name="fade" mode="out-in" appear>
-      <div v-if="answer === correct" key="correct">
+      <div v-if="answer === correct" key="correct" class="relative">
         <p class="font-fancy">Success!</p>
-        <p class="text-sm font-bold animate-bounce">Click to continue</p>
+        <p class="text-sm font-bold animate-bounce absolute left-0 right-0">Click to continue</p>
       </div>
-      <div v-else-if="answer === incorrect" key="incorrect">
+      <div v-else-if="answer === incorrect" key="incorrect" class="relative">
         <p class="font-fancy">Not even close!</p>
-        <p class="text-sm font-bold animate-bounce">Click to continue</p>
+        <p class="text-sm font-bold animate-bounce absolute left-0 right-0">Click to continue</p>
       </div>
       <div v-else-if="answer === loading" key="incorrect">
         <p class="font-fancy">Fetching cards..</p>
       </div>
-      <div v-else key="unanswered">
+      <div v-else key="unanswered" class="relative">
         <p class="font-fancy">What's this card?</p>
+        <p class="text-sm font-bold absolute left-0 right-0">Select answer below</p>
       </div>
     </transition>
   </div>
